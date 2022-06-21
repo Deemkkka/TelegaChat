7af42512e8d6b30eadc693e207e25b2ad475608e
Deem <Deem@DEEM>
2022-06-21 10:31:28 +0300

    Добавьте файлы проекта.

---
+++ TelegaChat/MVVM/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegaChat.MVVM.ViewModels.Base
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public bool Set<T>(ref T field, T value, [CallerMemberName] string prop = "")
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(prop);
+            return true;
+        }
+    }
+}
---
+++ TelegaChat/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegaChat.MVVM.Infrastructure.Commands;
+using TelegaChat.Net;
+
+namespace TelegaChat.MVVM.ViewModels
+{
+    internal class MainViewModel
+    {
+        public LambdaCommand ConnectToServerCommand { get; set; }
+
+        private Server _server;
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new LambdaCommand(o => _server.ConnectToServer());
+        }
+    }
+}
